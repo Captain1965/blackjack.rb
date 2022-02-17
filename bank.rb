6b440e39889c3b44760979937ddc0d9ec3055905
Captain1965 <alex1965-03@mail.ru>
2022-02-17 15:39:30 +0300

    create Class Bank for each participant

---
+++ bank.rb
@@ -0,0 +1,24 @@
+class Bank
+attr_reader :balance
+
+  def initialize
+    @balance = 100
+  end
+
+  def increase_balance
+    @balance +=10
+  end
+
+  def decrease_balance
+   @balance -=10 if @balance >0
+  end
+end
+
+#Test
+
+test = Bank.new
+puts test.balance
+test.increase_balance
+puts test.balance
+test.decrease_balance
+puts test.balance
