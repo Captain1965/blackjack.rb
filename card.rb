c94df6fb248655f7e0ea2a01c7ff7bc566e3c93f
Captain1965 <alex1965-03@mail.ru>
2022-02-18 15:09:29 +0300

    creat class table, this class include desk card , creat array cards and shufle this desk cards

--- card.rb
+++ card.rb
@@ -1,1 +1,19 @@
 class Card
+attr_reader :suit, :points, :card
+
+  def initialize (card,suit)
+    @card = card
+    @suit = suit
+    @points = points
+  end
+
+  def open_card
+    puts "#{card},#{suit}"
+  end
+
+  def point
+    returne 11 if card == "A"
+    returne 10 if ['K', 'Q', 'J'].include?(@card)
+  end
+end
+#Test
---
+++ table.rb
@@ -0,0 +1,24 @@
+class Table
+ attr_reader :cards
+
+ def initialize
+  @cards = Table.create_desk_cards
+ end
+
+  def self.create_desk_cards
+    cards = []
+    ['♥', '♦', '♣', '♠'].each do |suit|
+      (2..10).each do |number|
+        cards << Card.new(suit, number)
+      end
+
+      ['J', 'Q', 'K', 'A'].each do |card|
+        cards << Card.new(suit, card)
+      end
+    end
+    cards.shuffle
+  end
+end
+
+
+
